9f95476caf19420d4854c2df4e6d9073b59af4ba
WilmerL2000 <87109442+WilmerL2000@users.noreply.github.com>
2022-09-07 15:59:28 -0600

    Last modification of models and migrations tables

---
+++ app/Models/Process.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Process extends Model
+{
+    use HasFactory;
+
+    
+    protected $fillable = [
+        'se_oid',
+        'se_name',
+        'name',
+        'visible',
+        'state',
+    ];
+}
