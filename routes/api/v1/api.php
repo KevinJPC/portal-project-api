6af126ba76959de42566216083519e9d7501e821
Kevin Pitti <kepc08@gmail.com>
2022-09-07 11:06:16 -0600

    config updates

--- routes/api/v1/api.php
+++ routes/api/v1/api.php
@@ -17,3 +17,5 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+// Route::prefix('/')->group(base_path('routes/api/v1/Routes.php'));
