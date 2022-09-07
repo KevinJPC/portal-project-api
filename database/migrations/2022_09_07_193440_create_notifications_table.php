5635cc43b7a5db6f03c1f6e333f95ab740ba929f
WilmerL2000 <87109442+WilmerL2000@users.noreply.github.com>
2022-09-07 14:47:21 -0600

    Was modified the migration of notification

--- database/migrations/2022_09_07_193440_create_notifications_table.php
+++ database/migrations/2022_09_07_193440_create_notifications_table.php
@@ -16,7 +16,7 @@ return new class extends Migration
         Schema::create('notifications', function (Blueprint $table) {
             $table->id();
             $table->string('description');
-            $table->foreignId('user_has_process_id')->constrained('user_has_process_id')->onDelete('cascade');
+            $table->foreignId('user_has_process_id')->constrained()->onDelete('cascade');
             $table->timestamps();
         });
     }
