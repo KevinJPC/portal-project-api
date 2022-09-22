ddf1bb69e1dff71fe3348033094394217cbff485
Roig Antony Torres López <72164449+antonytorres21@users.noreply.github.com>
2022-09-22 11:43:02 -0600

    make final changes in rolehasprocceses maintenance

--- routes/api/v1/roles-has-process.php
+++ routes/api/v1/roles-has-process.php
@@ -3,7 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\RoleshasProcessesController;
-Route::middleware('auth:sanctum')->group(function () {
+Route::middleware('auth:api')->group(function () {
 
     Route::post('/register', [RoleshasProcessesController::class, 'createRolehasProcesses']);
     Route::get('/allRoles', [RoleshasProcessesController::class, 'allRolesHasProcesses']);
