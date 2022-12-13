3fd0d7a495476632c581fc6a50b60b7d41d56251
WilmerL2000 <87109442+WilmerL2000@users.noreply.github.com>
2022-12-13 07:22:14 -0600

    Changing some request rules

--- routes/api/v1/userRoutes.php
+++ routes/api/v1/userRoutes.php
@@ -4,7 +4,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\UserController;
 
-Route::middleware(['auth:sanctum', 'restrictTo:user'])->group(function () {
+Route::middleware(['auth:sanctum'])->group(function () {
     Route::patch('/', [UserController::class, 'updateUser']);
     Route::patch('/update-password', [UserController::class, 'updatePassword']);
     Route::get('/{user:id}', [UserController::class, 'getUserById']);
